00c42716776b5495025445f335421a0bd4b8ec14
Vladimir Karin <Vladimir.Karin@gmail.com>
2024-04-06 17:06:16 +0300

    Refactored code in css and js files

--- 09-Omnifood-Optimizations/js/script.js
+++ 09-Omnifood-Optimizations/js/script.js
@@ -1,71 +1,52 @@
-// The Start
-// const myName = 'Vladimir Karin';
-// const h1 = document.querySelector('.heading-primary');
-
-// h1.addEventListener('click', function () {
-//     h1.textContent = myName;
-//     h1.style.backgroundColor = 'red';
-//     h1.style.padding = '5rem';
-// });
-
-// console.log('Hello World!');
-// console.log(myName);
-// console.log(h1);
-
 //Year Update
 const yearElement = document.querySelector('.year');
 const currentYear = new Date().getFullYear();
 yearElement.textContent = currentYear;
 
 // Make mobile navigation work
-const navigationButtonElement = document.querySelector('.btn-mobile-nav');
-const headerElement = document.querySelector('header');
-
-navigationButtonElement.addEventListener('click', function () {
+const toggleMobileNav = () => {
+    const headerElement = document.querySelector('header');
     headerElement.classList.toggle('nav-open');
-});
+};
+
+const navigationButtonElement = document.querySelector('.btn-mobile-nav');
+navigationButtonElement.addEventListener('click', toggleMobileNav);
 
 //Smooth Scrolling animation
-const allLinks = document.querySelectorAll('a:link');
+const smoothScroll = (e) => {
+    e.preventDefault();
+    const href = e.currentTarget.getAttribute('href');
 
-allLinks.forEach(function (link) {
-    link.addEventListener('click', function (e) {
-        e.preventDefault();
-        const href = link.getAttribute('href');
-
-        //Scroll back to the top
-        if (href === '#')
-            window.scrollTo({
-                top: 0,
-                behavior: 'smooth',
-            });
-
+    // Scroll back to the top
+    if (href === '#') {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+    } else if (href.startsWith('#')) {
         // Scroll to other links
-        if (href !== '#' && href.startsWith('#')) {
-            const sectionElement = document.querySelector(href);
-            sectionElement.scrollIntoView({ behavior: 'smooth' });
-        }
+        const sectionElement = document.querySelector(href);
+        sectionElement.scrollIntoView({ behavior: 'smooth' });
+    }
 
-        // CLose mobile navigation
-        if (link.classList.contains('main-nav-link'))
-            headerElement.classList.toggle('nav-open');
-    });
-});
+    // Close mobile navigation
+    const headerElement = document.querySelector('header');
+    if (e.currentTarget.classList.contains('main-nav-link')) {
+        headerElement.classList.toggle('nav-open');
+    }
+};
+
+const allLinks = document.querySelectorAll('a:link');
+allLinks.forEach((link) => link.addEventListener('click', smoothScroll));
 
 // Sticky Navigation
 const sectionHeroElement = document.querySelector('.section-hero');
 const observer = new IntersectionObserver(
-    function (entries) {
+    (entries) => {
         const entry = entries[0];
-        if (!entry.isIntersecting) {
-            document.body.classList.add('sticky');
-        }
-        if (entry.isIntersecting) {
-            document.body.classList.remove('sticky');
-        }
+        document.body.classList.toggle('sticky', !entry.isIntersecting);
     },
     {
-        // In the vieport
         root: null,
         threshold: 0,
         rootMargin: '-80px',
@@ -74,10 +55,9 @@ const observer = new IntersectionObserver(
 
 observer.observe(sectionHeroElement);
 
-///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
-    var flex = document.createElement('div');
+    const flex = document.createElement('div');
     flex.style.display = 'flex';
     flex.style.flexDirection = 'column';
     flex.style.rowGap = '1px';
@@ -86,63 +66,12 @@ function checkFlexGap() {
     flex.appendChild(document.createElement('div'));
 
     document.body.appendChild(flex);
-    var isSupported = flex.scrollHeight === 1;
-    flex.parentNode.removeChild(flex);
-    console.log(isSupported);
+    const isSupported = flex.scrollHeight === 1;
+    document.body.removeChild(flex);
 
-    if (!isSupported) document.body.classList.add('no-flexbox-gap');
+    if (!isSupported) {
+        document.body.classList.add('no-flexbox-gap');
+    }
 }
+
 checkFlexGap();
-
-// https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
-
-/*
-.no-flexbox-gap .main-nav-list li:not(:last-child) {
-  margin-right: 4.8rem;
-}
-
-.no-flexbox-gap .list-item:not(:last-child) {
-  margin-bottom: 1.6rem;
-}
-
-.no-flexbox-gap .list-icon:not(:last-child) {
-  margin-right: 1.6rem;
-}
-
-.no-flexbox-gap .delivered-faces {
-  margin-right: 1.6rem;
-}
-
-.no-flexbox-gap .meal-attribute:not(:last-child) {
-  margin-bottom: 2rem;
-}
-
-.no-flexbox-gap .meal-icon {
-  margin-right: 1.6rem;
-}
-
-.no-flexbox-gap .footer-row div:not(:last-child) {
-  margin-right: 6.4rem;
-}
-
-.no-flexbox-gap .social-links li:not(:last-child) {
-  margin-right: 2.4rem;
-}
-
-.no-flexbox-gap .footer-nav li:not(:last-child) {
-  margin-bottom: 2.4rem;
-}
-
-@media (max-width: 75em) {
-  .no-flexbox-gap .main-nav-list li:not(:last-child) {
-    margin-right: 3.2rem;
-  }
-}
-
-@media (max-width: 59em) {
-  .no-flexbox-gap .main-nav-list li:not(:last-child) {
-    margin-right: 0;
-    margin-bottom: 4.8rem;
-  }
-}
-*/
